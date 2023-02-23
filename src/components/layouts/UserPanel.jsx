feb3c2099556749b35006ae15aed555a1773117a
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-02-23 18:45:52 +0300

    Implemented nested routes in UserPanel layout

--- src/components/layouts/UserPanel.jsx
+++ src/components/layouts/UserPanel.jsx
@@ -1,16 +1,16 @@
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
-import DashboardPanel from "../DashboardPanel";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 
 const UserPanel = () => {
   const navigate = useNavigate();
+
   return (
     <div className=" bg-slate-200 w-screen h-screen font-inter ">
       <Navbar />
       <div className=" flex h-full">
         <Sidebar navigate={navigate} />
-        <DashboardPanel />
+        <Outlet context={{ navigate }} />
       </div>
     </div>
   );

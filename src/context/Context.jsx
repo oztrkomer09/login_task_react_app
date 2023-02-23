ff2e10cc8362185e2a0d2f645c1acdee09dd11c7
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-02-23 13:08:52 +0300

    Updated fetchUsers function and added delete user feature

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,6 +1,14 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import toast from "react-hot-toast";
-import { getDocs, collection, orderBy } from "firebase/firestore";
+// import { getDocs, collection, } from "firebase/firestore";
+import {
+  collection,
+  query,
+  onSnapshot,
+  orderBy,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
 const UserContext = createContext();
@@ -15,10 +23,15 @@ export const UserProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
 
   const fetchUsers = async () => {
-    const colRef = collection(db, "users");
-    const snapshots = await getDocs(colRef);
-    const userDocs = snapshots.docs.map((doc) => doc.data());
-    setUsers(userDocs);
+    const qry = query(collection(db, "users"));
+    onSnapshot(qry, (snapshot) => {
+      setUsers(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+      );
+    });
   };
 
   useEffect(() => {
@@ -27,7 +40,6 @@ export const UserProvider = ({ children }) => {
 
   // Email validation and navigation to UserPanel
   const handleLogin = ({ navigate }) => {
-    console.log(users);
     if (userMail.match(regex)) {
       toast.success("Login Succesfull!");
       setTimeout(() => {
@@ -38,6 +50,7 @@ export const UserProvider = ({ children }) => {
     }
   };
 
+  //Logout function
   const handleLogout = ({ navigate }) => {
     toast("Logout Succesfull!", {
       icon: "👋🏻",
@@ -46,7 +59,22 @@ export const UserProvider = ({ children }) => {
     navigate("/");
   };
 
-  const data = { userMail, setUserMail, handleLogin, handleLogout, users };
+  //Delete user function
+  const handleDelete = (id) => {
+    deleteDoc(doc(db, "users", id));
+    toast("User Deleted!", {
+      icon: "🗑️",
+    });
+  };
+
+  const data = {
+    userMail,
+    setUserMail,
+    handleLogin,
+    handleLogout,
+    users,
+    handleDelete,
+  };
 
   return <UserContext.Provider value={data}>{children}</UserContext.Provider>;
 };
